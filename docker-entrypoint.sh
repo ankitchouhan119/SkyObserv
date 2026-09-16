7584e6c0f9974233576e6c431e816de0cb88b170
travobuds <travobuds@gmail.com>
2026-09-16 20:54:47 +0530

    issue fixes

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 if [ -n "$DATABASE_URL" ]; then
   echo "[entrypoint] Applying database schema..."
   # RDS and some managed Postgres present cert chains Node does not trust by default.
-  NODE_TLS_REJECT_UNAUTHORIZED=0 npx prisma db push --skip-generate
+  NODE_TLS_REJECT_UNAUTHORIZED=0 npx prisma db push --skip-generate --accept-data-loss
 fi
 
 exec "$@"
